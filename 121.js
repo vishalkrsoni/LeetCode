8473df5c8fe159a0b705200dc7b750e3a167827e
vishalkrsoni <vishal.sony1@gmail.com>
2022-09-28 14:04:44 +0530

    maximum profit stocks sell

--- 121.js
+++ 121.js
@@ -1,10 +1,9 @@
 var maxProfit = function (prices) {
-  let maxProfit = 0,minPrice=Infinity
+  let maxProfit = 0, minPrice = Infinity
   for (let price of prices) {
-        minPrice=Math.min(price,minPrice)
-        let profit=price-minPrice
-        maxProfit=Math.max(profit,maxProfit)
+    minPrice = Math.min(price, minPrice)
+    let profit = price - minPrice
+    maxProfit = Math.max(profit, maxProfit)
   }
-
   return maxProfit
 };
