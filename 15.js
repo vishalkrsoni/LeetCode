48fe2767d650cea87fc04bc70862406cd132da7e
vishalkrsoni <vishal.sony1@gmail.com>
2022-09-28 08:16:22 +0530

    three-sum

--- 15.js
+++ 15.js
@@ -1,17 +1,27 @@
 
-let nums = [-1, 0, 1, 2, -1, -4] //Output: [[-1,-1,2],[-1,0,1]]
+let nums = [-1, 0, 1, 2, -1, -4,] //Output: [[-1,-1,2],[-1,0,1]]
 var threeSum = (nums) => {
-  let output = {}, result = []
+  if (nums.length === 0) return []
+  let result = []
+  nums = nums.sort((a, b) => a - b)
   for (let i = 0; i < nums.length - 2; i++) {
-    for (let j = i + 1; j < nums.length - 1; j++) {
-      for (let k = j + 1; k < nums.length; k++) {
-        if (nums[i] + nums[j] + nums[k] == 0)
-          output[[nums[i], nums[j], nums[k]].sort((a, b) => a - b)] =
-            [nums[i], nums[j], nums[k]]
-      }
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let left = i + 1
+    let right = nums.length - 1
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right]
+      if (sum === 0) {
+        result.push([nums[i], nums[left], nums[right]])
+        while (nums[left] === nums[left + 1]) left++;
+        while (nums[right] === nums[right + 1]) right--;
+        left++;
+        right--;
+      } 
+      else if (sum < 0) left++
+      else right--;
     }
   }
-  return Object.values(output)
-};
+  return result
+}
 console.log(threeSum(nums))
 
