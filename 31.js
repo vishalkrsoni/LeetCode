59e637faaf7cee32eb5841da8bf6d7e24016efa8
vishalkrsoni <vishal.sony1@gmail.com>
2022-10-01 14:22:30 +0530

    next permutation

--- 31.js
+++ 31.js
@@ -1,16 +1,18 @@
 var nextPermutation = function (nums) {
   let i = nums.length - 2;
   while (i >= 0 && nums[i + 1] <= nums[i]) i--;
+  
   if (i >= 0) {
-    let j = nums.length - 1;
-    while (j >= 0 && nums[j] <= nums[i]) j--;
-    [nums[i], nums[j]] = [nums[j], nums[i]];
+    let end = nums.length - 1;
+    while (end >= 0 && nums[end] <= nums[i]) end--;
+    [nums[i], nums[end]] = [nums[end], nums[i]];
   }
-  let k = i + 1, n = nums.length - 1;
-  while (k < n) {
-    [nums[k], nums[n]] = [nums[n], nums[k]];
-    k++;
-    n--
+
+  let left = i + 1, right = nums.length - 1;
+  while (left < right) {
+    [nums[left], nums[right]] = [nums[right], nums[left]];
+    left++;
+    right--;
   }
   return nums
 };
