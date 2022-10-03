dc8076810937fa261ef27233ab5829a4f016a888
vishalkrsoni <vishal.sony1@gmail.com>
2022-10-04 00:35:42 +0530

    decode

---
+++ 394.js
@@ -0,0 +1,31 @@
+// let s = "3[a]2[bc]"
+// var decodeString = function (s) {
+//   let s1 = s.split('[').toString();
+//   let values = [], message = ''
+//   s1 = s1.split(']')
+//   for (val of s1)
+//     if (val.length > 0)
+//       values.push(val.split(','))
+//   console.log(values[0][0], values[1][1])
+
+//   for (let i = 0; i < values.length; i++) {
+//     num = values[i][0]
+//     val = values[i][1]
+//     while (num > 0) {
+//       message += val
+//       num--;
+//     }
+//   }
+//   return message
+// }
+// console.log(decodeString(s))
+
+const decodeString = str => {
+  let num, val = ''
+  let result = '', str1 = []
+  for(let char of s){
+    if(!isNaN(char)){
+      
+    }
+  }
+}
