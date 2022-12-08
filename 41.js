e93bfa015d4749d25420cfabdb02574a3cdb9754
vishalkrsoni <vishal.sony1@gmail.com>
2022-12-08 11:17:04 +0530

    first missing

---
+++ 41.js
@@ -0,0 +1,17 @@
+var firstMissingPositive = function (nums) {
+  let firstMissing = 1
+  let posNums = nums.filter(num => num > 0)
+  let max = Math.max(...posNums)
+  let numSet = new Set(posNums)
+  
+  while (firstMissing <= max) {
+    if (!numSet.has(firstMissing)) return firstMissing
+    firstMissing++
+  }
+  return firstMissing
+};
+
+let nums = [7, 8, 9, 11, 12]
+let n = [3, 4, -1, 1]
+console.log(firstMissingPositive(n))
+console.log(firstMissingPositive(nums))
