5270be4f41f983774c51e004d701f069d5c93ae3
vishalkrsoni <vishal.sony1@gmail.com>
2022-12-27 03:31:00 +0530

    new

--- 392.js
+++ 392.js
@@ -1,14 +1,18 @@
 var isSubsequence = function (s, t) {
   t = t.split('')
   s = s.split('')
-  l = s.length - 1
-  for (let i = 0; i < l; i++) {
-    let currIndex = t.indexOf(s[i])
-    let nextIndex = t.indexOf(s[i])
-    let lastIndex = t.indexOf(s[i])
-if(t.includes(s[i]) && t.includes(s[l]))
-    if (t.indexOf(s[i]) < t.indexOf(s[i + 1])) return true
+  len = s.length - 1
+  let currPos=0;
+  for (let i = 0; i < len; i++) {
+      if(t.includes(s[i])&& t.includes(s[len])){
+if (t.indexOf(s[i], currPos) != -1) {
+      currPos = t.indexOf(s[i])
+    } else {
+return false
+    }
+      }
+    
   }
-  return false
+  return true
 }
-console.log(isSubsequence('acd', 'aghbdgh'))
+console.log(isSubsequence('abc', 'ahbgdc'))
