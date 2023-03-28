840638313eea7d2c4ce6c8bc4191451f5103baab
vishalkrsoni <vishal.sony1@gmail.com>
2023-03-28 17:07:21 +0530

    removing repeated nodes and keep unique values

--- 82.js
+++ 82.js
@@ -4,19 +4,46 @@ class ListNode {
     this.next = next === undefined ? null : next;
   }
 }
+
+function removeGivenNode(head, node) {
+  if (head === node) {
+    return head.next;
+  }
+  let curr = head;
+  while (curr !== null) {
+    if (curr.next === node) {
+      curr.next = node.next;
+      break;
+    }
+    curr = curr.next;
+  }
+  return head;
+}
+
 var deleteDuplicates = function (head) {
   const trackedNodes = new Set();
-  console.log(trackedNodes)
+  let curr = head;
+  let prev = null;
+  while (curr !== null) {
+    if (trackedNodes.has(curr.val)) {
+      prev.next = curr.next;
+    } else {
+      trackedNodes.add(curr.val);
+      prev = curr;
+    }
+    curr = curr.next;
+  }
+  return head;
 };
-// 1 2 3 3 4 4 5
-const node1 = new ListNode(5);
-const node2 = new ListNode(7, node1);
-const node3 = new ListNode(4, node2);
-const node4 = new ListNode(3, node3);
-const node5 = new ListNode(8, node4);
-const node6 = new ListNode(8, node5);
-const node7 = new ListNode(1, node6);
 
-const newHead = deleteDuplicates(node7);
+const node1 = new ListNode(3);
+const node2 = new ListNode(3, node1);
+const node3 = new ListNode(2, node2);
+const node4 = new ListNode(1, node3);
+const node5 = new ListNode(1, node4);
+// const node6 = new ListNode(8, node5);
+// const node7 = new ListNode(1, node6);
+
+const newHead = deleteDuplicates(node5);
 
 console.log(newHead);
